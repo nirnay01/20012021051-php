931a1e116ceb1fee2b07f1f7567a9deac59517bf
nirnay01 <110597836+nirnay01@users.noreply.github.com>
2023-02-20 17:52:29 +0530

    up to date

--- pr4_1.php
+++ pr4_1.php
@@ -1,10 +1,15 @@
 <!-- 7.	Write a PHP program to print an arithmetic progression series.  -->
 <?php
-$first = 1;
-$last = 10;
-$diff = 2;
+function AP($a,$b,$d){
+$first = $a;
+$last = $b;
+$diff = $d;
 for($i = $first; $i <= $last; $i += $diff)
 {
 echo $i." ";
 }
+}
+AP(200,250,10);
+echo "</br>";
+AP(10,100,20);
 ?>
